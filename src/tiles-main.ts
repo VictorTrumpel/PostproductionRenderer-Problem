36bcd200fa7c6692c845341e177cbc6d3fd965ca
Victor Trumpel <victor.trumpel@gmail.com>
2024-07-05 01:01:11 +0300

    добавил пример с березиным

--- src/tiles-main.ts
+++ src/tiles-main.ts
@@ -1,9 +1,10 @@
 import Stats from "stats.js";
 import * as BUI from "@thatopen/ui";
 import * as OBC from "@thatopen/components";
+import * as OBCF from "@thatopen/components-front";
 
 (async () => {
-  const container = document.createElement("button");
+  const container = document.createElement("div");
   container.classList.add("ifc-div-container");
   document.body.append(container);
 
@@ -23,112 +24,36 @@ import * as OBC from "@thatopen/components";
 
   components.init();
 
-  world.camera.controls.setLookAt(12, 6, 8, 0, 0, -10);
-
   world.scene.setup();
+
+  world.camera.controls.setLookAt(12, 6, 8, 0, 0, -10);
 
   const grids = components.get(OBC.Grids);
   grids.create(world);
 
-  world.scene.three.background = null;
+  const loader = components.get(OBCF.IfcStreamer);
+  loader.world = world;
 
-  const tiler = components.get(OBC.IfcGeometryTiler);
+  loader.url = "http://localhost:5173/berezin-tiles/";
 
-  const wasm = {
-    path: "https://unpkg.com/web-ifc@0.0.55/",
-    absolute: true,
-  };
-
-  tiler.settings.wasm = wasm;
-  tiler.settings.minGeometrySize = 20;
-  tiler.settings.minAssetsSize = 1000;
-
-  let files: { name: string; bits: (Uint8Array | string)[] }[] = [];
-  let geometriesData: OBC.StreamedGeometries = {};
-  let geometryFilesCount = 1;
-
-  tiler.onGeometryStreamed.add((geometry) => {
-    const { buffer, data } = geometry;
-    const bufferFileName = `small.ifc-processed-geometries-${geometryFilesCount}`;
-    for (const expressID in data) {
-      const value = data[expressID];
-      value.geometryFile = bufferFileName;
-      geometriesData[expressID] = value;
+  async function loadModel(geometryURL: string, propertiesURL?: string) {
+    const rawGeometryData = await fetch(geometryURL);
+    const geometryData = await rawGeometryData.json();
+    let propertiesData;
+    if (propertiesURL) {
+      const rawPropertiesData = await fetch(propertiesURL);
+      propertiesData = await rawPropertiesData.json();
     }
-    files.push({ name: bufferFileName, bits: [buffer] });
-    geometryFilesCount++;
-  });
 
-  let assetsData: OBC.StreamedAsset[] = [];
-
-  tiler.onAssetStreamed.add((assets) => {
-    assetsData = [...assetsData, ...assets];
-  });
-
-  tiler.onIfcLoaded.add((groupBuffer) => {
-    files.push({
-      name: "small.ifc-processed-global",
-      bits: [groupBuffer],
-    });
-  });
-
-  function downloadFile(name: string, ...bits: (Uint8Array | string)[]) {
-    const file = new File(bits, name);
-    const anchor = document.createElement("a");
-    const url = URL.createObjectURL(file);
-    anchor.href = url;
-    anchor.download = file.name;
-    anchor.click();
-    URL.revokeObjectURL(url);
+    const model = await loader.load(geometryData, true, propertiesData);
+    console.log(model);
   }
 
-  async function downloadFilesSequentially(
-    fileList: { name: string; bits: (Uint8Array | string)[] }[]
-  ) {
-    for (const { name, bits } of fileList) {
-      downloadFile(name, ...bits);
-      await new Promise((resolve) => {
-        setTimeout(resolve, 100);
-      });
-    }
-  }
+  await loadModel(
+    "http://localhost:5173/berezin-tiles/small.ifc-processed.json"
+  );
 
-  tiler.onProgress.add((progress) => {
-    if (progress !== 1) return;
-    setTimeout(async () => {
-      const processedData = {
-        geometries: geometriesData,
-        assets: assetsData,
-        globalDataFileId: "small.ifc-processed-global",
-      };
-      files.push({
-        name: "small.ifc-processed.json",
-        bits: [JSON.stringify(processedData)],
-      });
-      await downloadFilesSequentially(files);
-      assetsData = [];
-      geometriesData = {};
-      files = [];
-      geometryFilesCount = 1;
-    });
+  world.camera.controls.addEventListener("sleep", () => {
+    loader.culler.needsUpdate = true;
   });
-
-  async function processFile() {
-    const fetchedIfc = await fetch(
-      "http://localhost:5173/berezin-tiles/ber1.ifc"
-    );
-    const ifcBuffer = await fetchedIfc.arrayBuffer();
-    // We will need this information later to also convert the properties
-    const ifcArrayBuffer = new Uint8Array(ifcBuffer);
-    // This triggers the conversion, so the listeners start to be called
-    await tiler.streamFromBuffer(ifcArrayBuffer);
-  }
-
-  const button = document.createElement("button");
-  button.innerText = "Скачать tiles";
-  button.style.position = "absolute";
-  document.body.append(button);
-  button.onclick = () => {
-    processFile();
-  };
 })();
